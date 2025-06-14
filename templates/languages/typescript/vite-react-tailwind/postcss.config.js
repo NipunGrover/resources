005aa879a7a9e0a37f6b2c57bcc045e688444a0f
Nipun Grover <nipungrover2003@outlook.com>
2025-06-14 00:54:56 -0400

    - made changes to main css files to allow upgrade tool to work - changed plugin in the postcss file

--- templates/languages/typescript/vite-react-tailwind/postcss.config.js
+++ templates/languages/typescript/vite-react-tailwind/postcss.config.js
@@ -1,6 +1,5 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+// instead of postcss plugin, new dedicated Vite plugin is now used
+export default defineConfig({ plugins: [tailwindcss()] });
